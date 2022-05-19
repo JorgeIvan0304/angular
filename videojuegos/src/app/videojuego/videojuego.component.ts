ebab8ef7d9e85c5c25b9c7deca4fa04986986897
JorgeIvan0304 <elizadarkmoon@gmail.com>
2022-05-19 07:49:04 -0500

    version1

---
+++ videojuegos/src/app/videojuego/videojuego.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'videojuego',
+  templateUrl: './videojuego.component.html'
+ 
+})
+
+export class VideojuegoComponent{
+    constructor(){
+        console.log("Corriendo de manera adecuada")
+    }
+}
